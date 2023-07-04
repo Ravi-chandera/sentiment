d60b0404d5a13bed2748fb80b90986fb3230aca1
Ravi-chandera <110690059+Ravi-chandera@users.noreply.github.com>
2023-07-05 00:14:09 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-import streamlit as st
+from flask import Flask, render_template, request
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
 from scipy.special import softmax
+
+app = Flask(__name__)
 
 MODEL = "cardiffnlp/twitter-roberta-base-sentiment"
 tokenizer = AutoTokenizer.from_pretrained(MODEL)
@@ -17,19 +19,22 @@ def roberta_model(sample1):
     }
     return scoresDict
 
-# Streamlit app
-def main():
-    st.title("Sentiment Analysis with Roberta Model")
-    user_input = st.text_input("Enter text for sentiment analysis")
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/analyze', methods=['POST'])
+def analyze():
+    user_input = request.form['user_input']
     
     if user_input:
         scores = roberta_model(user_input)
         sentiment_labels = ["Negative", "Neutral", "Positive"]
         sentiment_probs = softmax(list(scores.values()))
         
-        st.subheader("Sentiment Analysis Results:")
-        for label, prob in zip(sentiment_labels, sentiment_probs):
-            st.write(f"{label}: {prob:.2%}")
+        return render_template('results.html', labels=sentiment_labels, probs=sentiment_probs)
     
+    return render_template('index.html')
+
 if __name__ == "__main__":
-    main()
+    app.run(debug=True)
